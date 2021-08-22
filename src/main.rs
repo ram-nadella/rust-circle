c5d957318bdf91410a2e06ea24f0d860ced5ad36
Ram Nadella <ramnadella@transfix.io>
2021-08-22 15:33:06 -0400

    Read additiona env vars (move logic to function)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
-    let circle_build_num =
-        std::env::var("CIRCLE_BUILD_NUM").map_err(|_| "CIRCLE_BUILD_NUM env var is not set")?;
-    println!("Value of CIRCLE_BUILD_NUM env var is: {}", circle_build_num);
+    read_and_print_env_var("CIRCLE_BUILD_NUM")?;
+    read_and_print_env_var("CIRCLE_PR_USERNAME")?;
+    read_and_print_env_var("CUSTOM_PROJECT_VAR")?;
+    Ok(())
+}
+
+fn read_and_print_env_var(env_var_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let env_var_value =
+        std::env::var(env_var_name).map_err(|_| format!("{} env var is not set", env_var_name))?;
+    println!("Value of {} env var is: {}", env_var_name, env_var_value);
     Ok(())
 }
