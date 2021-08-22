12594103621d4facb011445c870d6b4eb5539b0a
Ram Nadella <ramnadella@transfix.io>
2021-08-22 15:25:05 -0400

    Read a circle env var

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+    let circle_build_num =
+        std::env::var("CIRCLE_BUILD_NUM").map_err(|_| "CIRCLE_BUILD_NUM env var is not set")?;
+    println!("Value of CIRCLE_BUILD_NUM env var is: {}", circle_build_num);
+    Ok(())
 }
